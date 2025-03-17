fd8a721e745c1d2df3a919becfc9e6701ed74a49
Federico <fedeelami@gmail.com>
2025-03-17 17:19:54 -0300

    correccion de errores y agregacion de puntuacion

--- questions.py
+++ questions.py
@@ -5,11 +5,9 @@ import sys
 
 
 # Preguntas para el juego
-
-respuestas = [1,2,3,4]
-
 questions = [
-        "¿Qué función se usa para obtener la longitud de una cadena en Python?",            "¿Cuál de las siguientes opciones es un número entero en Python?",
+        "¿Qué función se usa para obtener la longitud de una cadena en Python?",
+        "¿Cuál de las siguientes opciones es un número entero en Python?",
         "¿Cómo se solicita entrada del usuario en Python?", 
         "¿Cuál de las siguientes expresiones es un comentario válido en Python?",
         "¿Cuál es el operador de comparación para verificar si dos valores son iguales?",
@@ -29,6 +27,9 @@ answers = [
     ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+
+# Puntuacion del jugador
+puntuacion = 0
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
     
@@ -51,11 +52,16 @@ for _ in range(3):
         # Se verifica si la respuesta es correcta
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
+            puntuacion += 1
             break
         else:
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
-            print("Incorrecto. La respuesta correcta es:")
-            print(answers[question_index][correct_answers_index[question_index]])
+            print("Incorrecto")
+            if (puntuacion >= 0.5):
+                puntuacion -= 0.5
+            if intento == 1:    
+                print(f"la respuesta correcta era: {answers[question_index][correct_answers_index[question_index]]}")
     # Se imprime un blanco al final de la pregunta
 print()
+print(f"tu puntuacion final fue: {puntuacion}")
